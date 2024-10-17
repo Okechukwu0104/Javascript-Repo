852efaf8729ba12101f075fafd17d80a7727d126
Okechukwu Peter <okechukwupeter10@gmail.com>
2024-10-17 11:13:32 +0100

    phase 1

--- Phase gate/factorial.js
+++ Phase gate/factorial.js
@@ -2,15 +2,14 @@ const ps = require("prompt-sync")
 const prompt = ps()
 
 const value = parseInt(prompt("input a number to factor: "));
-	if(value <= 1000){
 	
 		let factorial = 1;
 		for(let count = 1; count <=value; count++){
 			factorial *= count
-		
 		}
-		console.log(factorial)
 
+	if(factorial <= 1000){
+		console.log(factorial)
 	}else{
-		console.log("Input a value <= 1000");
+		console.log("The result exceeds 1000");
 	}
